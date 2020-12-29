a3cc5d04702de82c45199bbd764a629a06596032
kogonidze <kottok1998@gmail.com>
2020-12-29 13:26:44 +0300

    styled app/header/homepage components with Emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import './App.css';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 
-function App() {
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+import { fontFamily, fontSize, gray2 } from './Styles';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
       <Header />
       <HomePage />
     </div>
   );
-}
+};
 
 export default App;
--- src/Icons.tsx
+++ src/Icons.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
 import user from './user.svg';
 
-export const UserIcon = () => <img src={user} alt="User" width="12px" />;
+export const UserIcon = () => (
+  <img
+    src={user}
+    alt="User"
+    css={css`
+      width: 12px;
+      opacity: 0.6;
+    `}
+  />
+);
