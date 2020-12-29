8fb0bffbdcdbd94e618e24a007b848093fbf518a
kogonidze <kottok1998@gmail.com>
2020-12-29 17:53:04 +0300

    added props and states

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,36 +1,62 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
 import { PrimaryButton } from './Styles';
+import { QuestionList } from './QuestionList';
+import { getUnansweredQuestions, QuestionData } from './QuestionsData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
 
-export const HomePage = () => (
-  <div
-    css={css`
-      margin: 50px auto 20px auto;
-      padding: 30px 20px;
-      max-width: 600px;
-    `}
-  >
-    <div
-      css={css`
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-      `}
-    >
-      <h2
+export const HomePage = () => {
+  const [questions, setQuestions] = useState<QuestionData[] | null>(null);
+  const [questionsLoading, setQuestionsLoading] = useState(true);
+  useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions);
+      setQuestionsLoading(false);
+    };
+    doGetUnansweredQuestions();
+  });
+  const handleAskQuestionClick = () => {
+    console.log('TODO - move to the AskPage');
+  };
+  return (
+    <Page>
+      <div
         css={css`
-          font-size: 15px;
-          font-weight: bold;
-          margin: 10px 0px 5px;
-          text-align: center;
-          text-transform: uppercase;
+          margin: 50px auto 20px auto;
+          padding: 30px 20px;
+          max-width: 600px;
         `}
       >
-        Unanswered Questions
-      </h2>
-      <PrimaryButton>Ask a question</PrimaryButton>
-    </div>
-  </div>
-);
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+          `}
+        >
+          <PageTitle>Unanswered Questions </PageTitle>
+          <PrimaryButton onClick={handleAskQuestionClick}>
+            Ask a question
+          </PrimaryButton>
+        </div>
+        {questionsLoading ? (
+          <div
+            css={css`
+              font-size: 16px;
+              font-style: italic;
+            `}
+          >
+            Loading
+          </div>
+        ) : (
+          <QuestionList data={questions || []} />
+        )}
+      </div>
+    </Page>
+  );
+};
