dbbcfa01fee7c715919b9bd5245a17dc1a306a7a
kogonidze <kottok1998@gmail.com>
2021-01-15 17:28:48 +0300

    created authContext and SignInPage

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import HomePage from './HomePage';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
+import { SignOutPage } from './SignOutPage';
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
@@ -13,6 +14,7 @@ import { fontFamily, fontSize, gray2 } from './Styles';
 import { NotFoundPage } from './NotFoundPage';
 import { QuestionPage } from './QuestionPage';
 import { HeaderWithRouter as Header } from './Header';
+import { AuthProvider } from './Auth';
 
 const AskPage = lazy(() => import('./AskPage'));
 
@@ -21,41 +23,58 @@ const store = configureStore();
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <div
-          css={css`
-            font-family: ${fontFamily};
-            font-size: ${fontSize};
-            color: ${gray2};
-          `}
-        >
-          <Header />
-          <Switch>
-            <Redirect from="/home" to="/" />
-            <Route exact path="/" component={HomePage} />
-            <Route path="/search" component={SearchPage} />
-            <Route path="/ask">
-              <Suspense
-                fallback={
-                  <div
-                    css={css`
-                      margin-top: 100px;
-                      text-align: center;
-                    `}
-                  >
-                    Loading...
-                  </div>
-                }
-              >
-                <AskPage />
-              </Suspense>
-            </Route>
-            <Route path="/signin" component={SignInPage} />
-            <Route path="/questions/:questionId" component={QuestionPage} />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <div
+            css={css`
+              font-family: ${fontFamily};
+              font-size: ${fontSize};
+              color: ${gray2};
+            `}
+          >
+            <Header />
+            <Switch>
+              <Redirect from="/home" to="/" />
+              <Route exact path="/" component={HomePage} />
+              <Route path="/search" component={SearchPage} />
+              <Route path="/ask">
+                <Suspense
+                  fallback={
+                    <div
+                      css={css`
+                        margin-top: 100px;
+                        text-align: center;
+                      `}
+                    >
+                      Loading...
+                    </div>
+                  }
+                >
+                  <AskPage />
+                </Suspense>
+              </Route>
+              <Route
+                path="/signin"
+                render={() => <SignInPage action="signin" />}
+              />
+              <Route
+                path="/signin-callback"
+                render={() => <SignInPage action="signin-callback" />}
+              />
+              <Route
+                path="/signout"
+                render={() => <SignOutPage action="signout" />}
+              />
+              <Route
+                path="/signout-callback"
+                render={() => <SignOutPage action="signout-callback" />}
+              />
+              <Route path="/questions/:questionId" component={QuestionPage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
     </Provider>
   );
 };
